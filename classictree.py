2ddb5dba6e369250bcf320d0ac7b561891db29e3
fedoseevtaf <76451152+fedoseevtaf@users.noreply.github.com>
2021-01-18 17:46:19 +0300

    Add random tree generation.

--- classictree.py
+++ classictree.py
@@ -1,18 +1,30 @@
 #==============================================================#
-generationsCount = 6
+generationsCount = 10
 
-angle = 90
+angle = 30
 lenbranch = 100
+
+randomKoef = 0.16
 
 #==============================================================#
 def angleChanger(angle):
-    pass
+    return angle
 
 def lenChanger(lenbranch):
-    pass
+    return lenbranch
+
+
+def stableLenChanger(lenbranch):
+    return int(lenbranch * 0.8)
+
+def randomLenChanger(lenbranch):
+    return int((random() ** randomKoef) * (1 - randomKoef) * lenbranch)
+
+lenChanger = stableLenChanger
 
 #==============================================================#
 import turtle as tp
+from random import random
 
 #==============================================================#
 tp.left(90)
@@ -23,4 +35,23 @@ tp.pendown()
 
 #==============================================================#
 def tree(angle, lenbranch, *skipArgs, count = 0, **skipKeyargs):
-    pass
+    if count >= generationsCount:
+        pass
+    else:
+        tp.forward(lenbranch)
+        tp.left(angle / 2)
+
+        newangle = angleChanger(angle)
+        newlen = lenChanger(lenbranch)
+        tree(newangle, newlen, count = count + 1)
+        
+        tp.right(angle)
+
+        newangle = angleChanger(angle)
+        newlen = lenChanger(lenbranch)
+        tree(newangle, newlen, count = count + 1)
+        
+        tp.left(angle / 2)
+        tp.backward(lenbranch)
+
+tree(angle, lenbranch)
